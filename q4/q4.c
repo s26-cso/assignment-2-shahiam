18b7656084785ff4f7f23c3d81c251ebb23df22a
Aaryan Shah <aaryan-shah@example.com>
2026-04-14 13:31:51 +0530

    q4.c

---
+++ q4/q4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<dlfcn.h>
+
+int main(){
+    char op[10];
+    int a,b;
+    char libname[20];
+
+    while(1){
+        int eof = scanf("%s %d %d",op,&a,&b);
+        sprintf(libname,"./lib%s.so",op);
+
+        void*openlib = dlopen(libname,RTLD_LAZY);
+        if(!openlib){
+            continue;
+        }
+
+        int (*pointtoop)(int,int);
+        pointtoop = (int(*)(int,int))dlsym(openlib,op);
+        if(pointtoop){
+            int result = pointtoop(a,b);
+            printf("%d\n",result);
+        }
+        dlclose(openlib);
+    }
+
+    return 0;
+}
